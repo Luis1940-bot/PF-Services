4aa158a70e99ab0bf54f32547984e4323d8ee356
Luis Gimenez <luisconsultor@outlook.com>
2022-06-01 23:13:31 -0300

    commit create table user

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -3,43 +3,82 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define(
-    "user",
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        // autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      role: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    }
-    // {
-    //   // don't add the timestamp attributes (updatedAt, createdAt)
-    //   timestamps: false,
-    //   // If don't want createdAt
-    //   createdAt: false,
-    //   // If don't want updatedAt
-    //   updatedAt: false,
-    // }
-    // {
-    //   initialAutoIncrement: 9000,
-    // }
-  );
+  sequelize.define("user", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    adress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    tution: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    document: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    trainings: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    avalable: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    cvu: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    google_user: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    facebook_user: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    twiter_user: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    verified_user: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  });
 };
