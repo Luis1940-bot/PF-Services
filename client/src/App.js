e9e069b1e1b20f53702d1033b402d7899b6da6a8
ivang111 <99693011+ivang111@users.noreply.github.com>
2022-06-01 19:53:41 -0500

    Front raiz

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,11 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import LandingPage from "./components/LandingPage";
-import AddRecipe from "./components/AddRecipe";
-import RecipeDetails from "./components/RecipeDetails";
-import AddDiet from "./components/AddDiet";
-import About from "./components/About";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <React.Fragment>
-      <div className="App">
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/home" exact component={HomePage} />
-        <Route path="/newRecipe" exact component={AddRecipe} />
-        <Route path="/diet" exact component={AddDiet} />
-        <Route path="/recipeDetail/:id" component={RecipeDetails} />
-        <Route path="/about" exact component={About} />
-      </div>
-    </React.Fragment>
+    <div >
+      hola
+    </div>
   );
 }
 
