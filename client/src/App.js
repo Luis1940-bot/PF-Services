167663200fec9c69f50dd347378c2baab6459db5
Juan Prado <juan_prado_rojas@outlook.com>
2022-06-04 23:42:11 -0400

    Login Updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import { Route } from 'react-router-dom';
-import Landingpage from "./components/Landingpage/Landingpage.js"
+import { Route } from "react-router-dom";
+import Landingpage from "./components/Landingpage/Landingpage.js";
+import Login from "./components/LoginForm/Login.js";
+import Navbar from "./components/Navbar/Navbar.js";
 
 function App() {
   return (
-    <div >
+    <div>
+      <Navbar />
       <Route exact path="/" component={Landingpage} />
-      app.js
+      <Route exact path="/login" component={Login} />
     </div>
   );
 }
--- client/src/components/LoginForm/Login.js
+++ client/src/components/LoginForm/Login.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./Login.css";
-import Navbar from "../Navbar/Navbar";
+
 import image from "./helpImages/SideImage.png";
 import google from "./helpImages/google.png";
 import facebook from "./helpImages/facebook.png";
 import responsive from "./helpImages/responsive.png";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <>
-      <Navbar />
-
       <form className="form">
         <h1 className="title">Login</h1>
         <input
@@ -22,11 +20,12 @@ const Login = () => {
         <span className="error1">
           Please enter your user name or email again.
         </span>
-        <input className="inputs" type="text" placeholder="Password" />
+        <input className="inputs" type="password" placeholder="Password" />
         <span className="error2">Incorrect password, please try again.</span>
         <div>
-          <label class="labelCheck">
-            <input type="checkbox" checked="checked" />
+          <label className="labelCheck">
+            <input className="box" type="checkbox" />
+            <span className="checkmark"></span>
             Remember me.
           </label>
         </div>
