57b98709fc09cacf71aa1187ab3718efb54d9f58
Luis Gimenez <luisconsultor@outlook.com>
2022-06-19 00:03:07 -0300

    commit se agrega variable de entorno PORT

--- api/index.js
+++ api/index.js
@@ -19,12 +19,13 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+require("dotenv").config();
+const PORT = process.env.PORT;
 // Syncing all the models at once.
 //modifica => alter: true
 //desde cero => force:true
 conn.sync({}).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
