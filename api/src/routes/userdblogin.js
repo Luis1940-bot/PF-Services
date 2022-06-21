4455aa4ce6d65ce05a2c7e39b1e17b97d23cfb52
Luis Gimenez <luisconsultor@outlook.com>
2022-06-21 11:26:17 -0300

    commit cambio en jwtoken

--- api/src/routes/userdblogin.js
+++ api/src/routes/userdblogin.js
@@ -114,6 +114,8 @@ router.post("/userdblogin", userValidShortReg(), validate, async (req, res) => {
       res.cookie("userBackend", tokenBack, {
         expires: new Date(Date.now() + 3 * 60 * 60 * 1000), //3 hours expiration
         httpOnly: true,
+        sameSite: none,
+        secure: true,
       });
       // COOKIE FRONTEND
       res.cookie(
@@ -122,6 +124,8 @@ router.post("/userdblogin", userValidShortReg(), validate, async (req, res) => {
         {
           expires: new Date(Date.now() + 3 * 60 * 60 * 1000), //3 hours expiration
           httpOnly: false,
+          sameSite: none,
+          secure: true,
         }
       );
 
