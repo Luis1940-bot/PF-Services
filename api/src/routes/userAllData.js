82e2af6d74d7c3c4b8f3aaad861408a76811c671
Luis Gimenez <luisconsultor@outlook.com>
2022-06-21 15:06:25 -0300

    userProfessionalByID

--- api/src/routes/userAllData.js
+++ api/src/routes/userAllData.js
@@ -117,4 +117,46 @@ router.get("/userDetalleById/:id", async (req, res) => {
   }
 });
 
+router.get("/userProfessionalByID/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (id && Number.isInteger(parseInt(id))) {
+      const usersProfessionals = await db.Users.findAll({
+        where: { id: id },
+        include: [
+          {
+            model: db.Cities,
+            attributes: ["name"],
+            //required: true,
+          },
+          {
+            model: db.States,
+            attributes: ["name"],
+            //required: true,
+          },
+          {
+            model: db.Countries,
+            attributes: ["name"],
+            //required: true,
+          },
+          {
+            model: db.Professionals,
+            // required: true,
+          },
+        ],
+      });
+
+      if (usersProfessionals.length > 0) {
+        res.status(201).json(usersProfessionals);
+      } else {
+        res.status(422).json("Not found");
+      }
+    } else {
+      res.status(422).send("No envió un ID");
+    }
+  } catch (e) {
+    res.send(e);
+  }
+});
+
 module.exports = router;
