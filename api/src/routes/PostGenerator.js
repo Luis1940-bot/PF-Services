ca51e44de1a7cf5b833c181bb9d76ee0d03959f9
Luis Gimenez <luisconsultor@outlook.com>
2022-06-24 10:13:21 -0300

    se agrega datos de oferta a la consulta del posteo

--- api/src/routes/PostGenerator.js
+++ api/src/routes/PostGenerator.js
@@ -361,6 +361,22 @@ router.get("/posteosUsersByUserID/:id", async (req, res) => {
             attributes: ["name"],
             //required: true,
           },
+          {
+            model: db.Auctions,
+            attributes: ["date", "offer", "comment", "approved", "cancel"],
+            include: [
+              {
+                model: db.Professionals,
+                attributes: ["id", "trainings", "photo"],
+                // include: [
+                //   {
+                //     model: db.Users,
+                //     attributes: ["name", "surname", "age"],
+                //   },
+                // ],
+              },
+            ],
+          },
         ],
       });
       if (postFound.length > 0) {
@@ -375,5 +391,4 @@ router.get("/posteosUsersByUserID/:id", async (req, res) => {
     res.status(400).send(error);
   }
 });
-
 module.exports = router;
