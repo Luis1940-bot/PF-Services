4d9d8eea3cd6d548fe2d826696c24713db788634
Luis Gimenez <luisconsultor@outlook.com>
2022-06-24 10:57:50 -0300

    se acomodan message y conversations

--- api/src/routes/message.js
+++ api/src/routes/message.js
@@ -1,36 +1,43 @@
-const { Router} = require('express');
-const axios = require("axios")
-const {Message} = require('../db')
-const router = Router();
+const express = require("express");
+const router = express.Router();
+//const axios = require("axios");
+//const { Message } = require("../db");
 
+const cors = require("cors");
+router.use(
+  cors({
+    origin: true, //process.env.URL_CLIENT,
+    credentials: true,
+    //allowedHeaders: "Content-Type, Authorization",
+  })
+);
+router.use(express.json());
 
-router.post("/message", async (req,res)=>{
-    const {text , conversationId, sender} = req.body;
-    try{
-     const mensaje = await Message.create({
-        text: text,
+router.post("/message", async (req, res) => {
+  const { text, conversationId, sender } = req.body;
+  try {
+    const mensaje = await db.Message.create({
+      text: text,
+      conversationId: conversationId,
+      sender: sender,
+    });
+    res.status(200).json(mensaje);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+router.get("/allmessage/:conversationId", async (req, res) => {
+  const { conversationId } = req.params;
+  try {
+    const allmessage = await db.Message.findAll({
+      where: {
         conversationId: conversationId,
-        sender:sender
-      })
-      res.status(200).json(mensaje)
-    }
-    catch(err){
-      res.status(500).send(err)
-    }
-  })
-  router.get("/allmessage/:conversationId", async (req,res) =>{
-    const {conversationId} = req.params;
-    try{
-    const allmessage = await Message.findAll({
-      where:{
-        conversationId: conversationId
-      }
-    })
-    res.status(200).json(allmessage)
-  } catch(err){
-    res.send (error)
-  } 
-  })
+      },
+    });
+    res.status(200).json(allmessage);
+  } catch (err) {
+    res.send(error);
+  }
+});
 
-    
 module.exports = router;
