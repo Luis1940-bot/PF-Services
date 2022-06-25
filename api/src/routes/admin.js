07598de3d4c1ef15a4be3b35a43a0aa37a43e515
Luis Gimenez <luisconsultor@outlook.com>
2022-06-24 23:10:29 -0300

    admin

--- api/src/routes/admin.js
+++ api/src/routes/admin.js
@@ -604,9 +604,10 @@ router.put("/balance_1/:id", async (req, res) => {
 //?---------------------------------------------------------------
 
 //?**----CONTRACTS -------------------------------------
-router.get("/getContracts", async (req, res) => {
+router.get("/AllContractsActivo", async (req, res) => {
   try {
     const contracts = await db.Contracts.findAll({
+      where: { status: "activo" },
       attributes: ["id", "price", "date", "hour", "postId"],
       include: [
         {
@@ -691,15 +692,530 @@ router.get("/getContracts", async (req, res) => {
     res.status(400).send(error);
   }
 });
-
-//?**-------------------------------------------------------
-
-//?-----OFFERS----------
-router.get("/auctions", async (req, res) => {
+router.get("/AllContractsTerminado", async (req, res) => {
   try {
+    const contracts = await db.Contracts.findAll({
+      where: { status: "terminado" },
+      attributes: ["id", "price", "date", "hour", "postId"],
+      include: [
+        {
+          model: db.Auctions,
+          attributes: ["id", "offer", "comment", "approved"],
+
+          include: [
+            {
+              model: db.Posts,
+              attributes: [
+                "date_post",
+                "date_ini",
+                "date_fin",
+                "needs",
+                "active",
+                "locationReference",
+                "contact_phone",
+                "agePatient",
+                "namePatient",
+                "addressPatient",
+              ],
+              include: [
+                {
+                  model: db.Cities,
+                  attributes: ["name"],
+                },
+                {
+                  model: db.States,
+                  attributes: ["name"],
+                },
+                {
+                  model: db.Countries,
+                  attributes: ["name"],
+                },
+                {
+                  model: db.Users,
+                  attributes: ["name", "surname", "email", "phone"],
+                },
+              ],
+            },
+            {
+              model: db.Professionals,
+              attributes: ["cvu", "photo"],
+              include: [
+                {
+                  model: db.Users,
+                  attributes: [
+                    "name",
+                    "surname",
+                    "phone",
+                    "age",
+                    "document",
+                    "email",
+                  ],
+                  include: [
+                    {
+                      model: db.Cities,
+                      attributes: ["name"],
+                    },
+                    {
+                      model: db.States,
+                      attributes: ["name"],
+                    },
+                    {
+                      model: db.Countries,
+                      attributes: ["name"],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    if (contracts.length > 0) {
+      res.status(201).json(contracts);
+    } else {
+      res.status(422).json("Not found");
+    }
   } catch (error) {
     res.status(400).send(error);
   }
 });
+router.get("/AllContractsCancelado", async (req, res) => {
+  try {
+    const contracts = await db.Contracts.findAll({
+      where: { status: "cancelado" },
+      attributes: ["id", "price", "date", "hour", "postId"],
+      include: [
+        {
+          model: db.Auctions,
+          attributes: ["id", "offer", "comment", "approved"],
+
+          include: [
+            {
+              model: db.Posts,
+              attributes: [
+                "date_post",
+                "date_ini",
+                "date_fin",
+                "needs",
+                "active",
+                "locationReference",
+                "contact_phone",
+                "agePatient",
+                "namePatient",
+                "addressPatient",
+              ],
+              include: [
+                {
+                  model: db.Cities,
+                  attributes: ["name"],
+                },
+                {
+                  model: db.States,
+                  attributes: ["name"],
+                },
+                {
+                  model: db.Countries,
+                  attributes: ["name"],
+                },
+                {
+                  model: db.Users,
+                  attributes: ["name", "surname", "email", "phone"],
+                },
+              ],
+            },
+            {
+              model: db.Professionals,
+              attributes: ["cvu", "photo"],
+              include: [
+                {
+                  model: db.Users,
+                  attributes: [
+                    "name",
+                    "surname",
+                    "phone",
+                    "age",
+                    "document",
+                    "email",
+                  ],
+                  include: [
+                    {
+                      model: db.Cities,
+                      attributes: ["name"],
+                    },
+                    {
+                      model: db.States,
+                      attributes: ["name"],
+                    },
+                    {
+                      model: db.Countries,
+                      attributes: ["name"],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    if (contracts.length > 0) {
+      res.status(201).json(contracts);
+    } else {
+      res.status(422).json("Not found");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+router.put("/activoContract/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (id && Number.isInteger(parseInt(id))) {
+      await db.Contracts.update(
+        {
+          status: "activo",
+        },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      return res.status(200).send("Contrato activo");
+    } else {
+      return res.status(400).json({
+        error: "no se envió id",
+      });
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+router.put("/terminadoContract/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (id && Number.isInteger(parseInt(id))) {
+      await db.Contracts.update(
+        {
+          status: "terminado",
+        },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      return res.status(200).send("Contrato terminado");
+    } else {
+      return res.status(400).json({
+        error: "no se envió id",
+      });
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+router.put("/canceladoContract/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (id && Number.isInteger(parseInt(id))) {
+      await db.Contracts.update(
+        {
+          status: "cancelado",
+        },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      return res.status(200).send("Contrato cancelado");
+    } else {
+      return res.status(400).json({
+        error: "no se envió id",
+      });
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+//?**-------------------------------------------------------
+
+//?-----OFFERS-------------------------------------------
+router.get("/AllAuctions", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const posts = await db.Auctions.findAll({
+      where: { cancel: 0 },
+      attributes: ["id", "date", "offer", "comment"],
+      include: [
+        {
+          model: db.Posts,
+          attributes: [
+            "date_post",
+            "date_ini",
+            "date_fin",
+            "needs",
+            "locationReference",
+            "availableTime_0",
+            "availableTime_1",
+            "agePatient",
+            "namePatient",
+            "addressPatient",
+          ],
+
+          include: [
+            {
+              model: db.Cities,
+              attributes: ["name"],
+            },
+            {
+              model: db.States,
+              attributes: ["name"],
+            },
+            {
+              model: db.Countries,
+              attributes: ["name"],
+              //required: true,
+            },
+          ],
+        },
+
+        {
+          model: db.Professionals,
+        },
+      ],
+    });
+
+    if (posts.length > 0) {
+      res.status(201).json(posts);
+    } else {
+      res.status(422).json("Not found");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+router.get("/AllAuctionsNOApproved", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const posts = await db.Auctions.findAll({
+      where: { [Op.and]: [{ approved: 0 }, { cancel: 0 }] },
+      attributes: ["id", "date", "offer", "comment"],
+      include: [
+        {
+          model: db.Posts,
+          attributes: [
+            "date_post",
+            "date_ini",
+            "date_fin",
+            "needs",
+            "locationReference",
+            "availableTime_0",
+            "availableTime_1",
+            "agePatient",
+            "namePatient",
+            "addressPatient",
+          ],
+
+          include: [
+            {
+              model: db.Cities,
+              attributes: ["name"],
+            },
+            {
+              model: db.States,
+              attributes: ["name"],
+            },
+            {
+              model: db.Countries,
+              attributes: ["name"],
+              //required: true,
+            },
+          ],
+        },
+
+        {
+          model: db.Professionals,
+        },
+      ],
+    });
+
+    if (posts.length > 0) {
+      res.status(201).json(posts);
+    } else {
+      res.status(422).json("Not found");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+router.get("/AllAuctionsApproved", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const posts = await db.Auctions.findAll({
+      where: { [Op.and]: [{ approved: 1 }, { cancel: 0 }] },
+      attributes: ["id", "date", "offer", "comment"],
+      include: [
+        {
+          model: db.Posts,
+          attributes: [
+            "date_post",
+            "date_ini",
+            "date_fin",
+            "needs",
+            "locationReference",
+            "availableTime_0",
+            "availableTime_1",
+            "agePatient",
+            "namePatient",
+            "addressPatient",
+          ],
+
+          include: [
+            {
+              model: db.Cities,
+              attributes: ["name"],
+            },
+            {
+              model: db.States,
+              attributes: ["name"],
+            },
+            {
+              model: db.Countries,
+              attributes: ["name"],
+              //required: true,
+            },
+          ],
+        },
+
+        {
+          model: db.Professionals,
+        },
+      ],
+    });
+
+    if (posts.length > 0) {
+      res.status(201).json(posts);
+    } else {
+      res.status(422).json("Not found");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+router.get("/AllCancel", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const posts = await db.Auctions.findAll({
+      where: { cancel: 1 },
+      attributes: ["id", "date", "offer", "comment"],
+      include: [
+        {
+          model: db.Posts,
+          attributes: [
+            "date_post",
+            "date_ini",
+            "date_fin",
+            "needs",
+            "locationReference",
+            "availableTime_0",
+            "availableTime_1",
+            "agePatient",
+            "namePatient",
+            "addressPatient",
+          ],
+
+          include: [
+            {
+              model: db.Cities,
+              attributes: ["name"],
+            },
+            {
+              model: db.States,
+              attributes: ["name"],
+            },
+            {
+              model: db.Countries,
+              attributes: ["name"],
+              //required: true,
+            },
+          ],
+        },
+
+        {
+          model: db.Professionals,
+        },
+      ],
+    });
+
+    if (posts.length > 0) {
+      res.status(201).json(posts);
+    } else {
+      res.status(422).json("Not found");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+router.put("/cancelAuction/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (id && Number.isInteger(parseInt(id))) {
+      await db.Auctions.update(
+        {
+          cancel: 1,
+        },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      return res.status(200).send("Oferta cancelada");
+    } else {
+      return res.status(400).json({
+        error: "no se envió id",
+      });
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+router.put("/activeAuction/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (id && Number.isInteger(parseInt(id))) {
+      await db.Auctions.update(
+        {
+          cancel: 0,
+        },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      return res.status(200).send("Oferta activada");
+    } else {
+      return res.status(400).json({
+        error: "no se envió id",
+      });
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+//?--------------------------------------------------------
 
 module.exports = router;
