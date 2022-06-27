b6a66565c2d211b6958a39545766b4bec0cce914
Luis Gimenez <luisconsultor@outlook.com>
2022-06-27 17:09:56 -0300

    traerPostByProfessionalsByContract

--- api/src/routes/auctions.js
+++ api/src/routes/auctions.js
@@ -195,6 +195,66 @@ router.get("/traerPostByProfessionals/:id", async (req, res) => {
   }
 });
 
+router.get("/traerPostByProfessionalsByContract/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (id && Number.isInteger(parseInt(id))) {
+      const posts = await db.Auctions.findAll({
+        where: { professionalId: id },
+        attributes: ["id", "date", "offer", "comment"],
+        include: [
+          {
+            model: db.Posts,
+            attributes: [
+              "date_post",
+              "date_ini",
+              "date_fin",
+              "needs",
+              "locationReference",
+              "availableTime_0",
+              "availableTime_1",
+              "agePatient",
+              "namePatient",
+              "addressPatient",
+            ],
+            include: [
+              {
+                model: db.Cities,
+                attributes: ["name"],
+              },
+              {
+                model: db.States,
+                attributes: ["name"],
+              },
+              {
+                model: db.Countries,
+                attributes: ["name"],
+                //required: true,
+              },
+            ],
+            include: [
+              {
+                model: db.Contracts,
+              },
+            ],
+          },
+        ],
+      });
+
+      if (posts.length > 0) {
+        res.status(201).json(posts);
+      } else {
+        res.status(422).json("Not found");
+      }
+    } else {
+      res.status(422).send("No envió un ID");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 router.get("/auctions", async (req, res) => {
   const auctions = await db.Auctions.findAll({});
   res.json(auctions);
